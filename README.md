01083c78ce74b621398e121361ddb5494cd579ac
takeru <take.yama0519@gmail.com>
2023-11-19 00:29:01 +0900

    First Published

---
+++ README.md
@@ -0,0 +1,35 @@
+# LAMP Builder
+
+## Tested Linux Distributions
+
+* AlmaLinux 9.2
+
+## About
+
+This builder installs the following set of modules
+
+* Apache
+* MySQL
+* PHP
+
+This Builder configure the following PHP settings
+
+* date.timezone = Asia/Tokyo
+* memory_limit = 512M
+* post_max_size = 128M
+* upload_max_filesize = 128M
+
+This Builder configure the following Firewall settings
+
+* Allow HTTP
+* Allow HTTPS
+* Remove Cockpit Service
+* Remove dhcpv6-client Service
+
+## Usage
+
+Execute the following command as a user with root privileges.
+
+```
+curl -s https://raw.githubusercontent.com/Takeru-Yamamoto/LAMP-Builder/script.bash | bash
+```
---
+++ script.bash
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+
+# コメントの表示
+echo ""
+echo "--------------------------------------------------"
+echo "Start Building LAMP Environment."
+echo "--------------------------------------------------"
+echo ""
+
+# 各種インストール
+
+## Apacheのインストール
+dnf -y install httpd httpd-devel
+systemctl start httpd
+systemctl enable httpd
+
+### Firewallの設定
+firewall-cmd --remove-service=cockpit --permanent
+firewall-cmd --remove-service=dhcpv6-client --permanent
+firewall-cmd --add-port=80/tcp --permanent
+firewall-cmd --add-port=443/tcp --permanent
+firewall-cmd --reload
+
+## MySQLのインストール
+dnf -y install mysql mysql-server
+systemctl start mysqld
+systemctl enable mysqld
+
+## PHPのインストール
+dnf -y module install php:remi-8.2
+dnf -y install php \
+    php-cli \
+    php-common \
+    php-fpm \
+    php-mbstring \
+    php-xml \
+    php-bcmath \
+    php-devel \
+    php-gd \
+    php-intl \
+    php-mysqlnd \
+    php-opcache \
+    php-pdo \
+    php-pear \
+    php-pecl-apcu \
+    php-pecl-apcu-devel \
+    php-pecl-json-post \
+    php-pecl-mcrypt \
+    php-pecl-mysql \
+    php-pecl-xmlrpc \
+    php-pecl-zip \
+    php-pgsql \
+    php-soap \
+    php-json
+
+### PHPの設定
+
+## PHPの設定ファイルのバックアップ
+cp /etc/php.ini /etc/php.ini.bak
+
+## PHPの設定ファイルの編集
+
+### タイムゾーンの設定
+sed -i -e "s/;date.timezone =/date.timezone = Asia\/Tokyo/g" /etc/php.ini
+
+### メモリの設定
+sed -i -e "s/memory_limit = 128M/memory_limit = 512M/g" /etc/php.ini
+
+### post_max_size の設定
+sed -i -e "s/post_max_size = 8M/post_max_size = 128M/g" /etc/php.ini
+
+### upload_max_filesize の設定
+sed -i -e "s/upload_max_filesize = 2M/upload_max_filesize = 128M/g" /etc/php.ini
+
+### Apacheの再起動
+systemctl restart httpd
+
+# コメントの表示
+echo ""
+echo "--------------------------------------------------"
+echo "Complete Building LAMP Environment."
+echo "--------------------------------------------------"
+echo ""
